892aacccbc912bf50bae99292711eaaace755ca0
Aleksandr <aleksandrastapov87@gmail.com>
2015-12-05 20:15:01 +0200

    2015-12-05  Lesson #9 and abstract

--- www/index.php
+++ www/index.php
@@ -14,15 +14,14 @@
     <TITLE>Обучение WEB</TITLE>
   </HEAD>
   <BODY>
-    <div>
+
       <p>Занятие №1</p>
       <ul>
         <li><a href="si_lesson_1_1.php">Первый урок</a></li>
         <li><a href="si_homework_1_1.php">HomeWork #1</a></li>
         <li><a href="si_homework_1_2.php">HomeWork #1bonus</a></li>
       </ul>
-    </div>
-    <div>
+
       <p>Занятие №2</p>
       <ul>
         <li><a href="si_lesson_2_1.php">Второй урок, пример 1</a></li>
@@ -30,47 +29,47 @@
         <li><a href="si_homework_2_1.php">HomeWork #2_1</a></li>
         <li><a href="si_homework_2_2.php">HomeWork #2_2 - Туристический калькулятор</a></li>
       </ul>
-    </div>
-    <div>
+
       <p>Занятие №3</p>
       <ul>
         <li><a href="si_lesson_3_1.php">Третий урок</a></li>
         <li><a href="si_homework_3_1.php">HomeWork #3 - Процентный множитель</a></li>
       </ul>
-    </div>
-    <div>
+
       <p>Занятие №4</p>
       <ul>
         <li><a href="si_lesson_4_1.php">Четвёртый урок</a></li>
         <li><a href="si_homework_4_1.php">HomeWork #4 - Рекурсия</a></li>
       </ul>
-    </div>
-    <div>
+
       <p>Занятие №5</p>
       <ul>
         <li><a href="si_lesson_5_1.php">Пятый урок</a></li>
         <li><a href="si_homework_5_1.php">HomeWork #5_1 - Редактор</a></li>
         <li><a href="si_homework_5_2.php">HomeWork #5_2 - Редактор с выбором</a></li>
       </ul>
-    </div>
-    <div>
+
       <p>Занятие №6</p>
       <ul>
         <li><a href="si_lesson_6_1.html">Шестой урок</a></li>
       </ul>
-    </div>
-    <div>
+
       <p>Занятие №7</p>
       <ul>
         <li><a href="si_lesson_7_1.php">Седьмой урок</a></li>
         <li><a href="si_homework_7_1.php">HomeWork #7_1 - Математические функции</a></li>
       </ul>
-    </div>
-    <div>
+
       <p>Занятие №8</p>
       <ul>
         <li><a href="si_lesson_8_1.php">Восьмой урок</a></li>
+        <li><a href="si_homework_8_1.php">HomeWork #8 - Файловый менеджер</a></li>
       </ul>
-    </div>
+      
+      <p>Занятие №9</p>
+      <ul>
+        <li><a href="si_lesson_9_1.php">Девятый урок</a></li>
+      </ul>
+      
   </BODY>
 </HTML>
